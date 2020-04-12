f9e7bdf261539ad266d8b864430591e2495550f6
QoloQ <jj.honza@gmail.com>
2020-04-12 14:45:54 +0200

    Final commit for a while, script finaly working. Bye for a while

--- token_getter.ps1
+++ token_getter.ps1
@@ -1,3 +1,10 @@
+## This script is made to get authtoken from server which can by then
+## used for communication with server. Variable $data is string lookin
+## like json bc that is what this specific server expects.
+## I agree with public use of this code.
+
+
+
 param(
 	[string]$url = $(throw "-url required"),
 
@@ -9,18 +16,17 @@ param(
 	[Parameter(Mandatory=$False)]
 	[string]$jmeno = "jchmelarapi",
 
-	[Parameter(Mandatory=$False)]
-	[string]$heslo = (Read-Host -AsSecureString "password:") 
-	
-) 
-
-$heslo
-$pass = convertfrom-securestring $heslo.heslo
-$pass
+	[Security.SecureString]$heslo = (Read-Host -AsSecureString "password:") 
+	) 
 
 
+"converting from ss...."
+$pass = convertfrom-securestring $heslo -asplaintext
+"printin the answer..."
 
-$dataa = '"{\"grant_type\":\"password\",
+$pass 
+
+$data = '"{\"grant_type\":\"password\",
  \"client_id\":\"'+ $klient_id+'\",
   \"client_secret\":\"\",
   \"username\":\"'+ $jmeno+'\",
@@ -33,9 +39,8 @@ $finurl = $url + $module
 
 
 
-$token= (curl --request POST --url $finurl --header "Cache-Control:no-cache" --header "Content-Type: application/json" --header "Accept: application/json" --data $dataa | convertfrom-json | select -Property access_token).access_token
+$token= (curl --request POST --url $finurl --header "Cache-Control:no-cache" --header "Content-Type: application/json" --header "Accept: application/json" --data $data | convertfrom-json | select -Property access_token).access_token
 
-$tokk = $token.access_token
 $resp = curl -X GET -H OAuth-Token:$token https://dsugar.kapitol.cz/rest/v11/ping
 
-$resp
+
